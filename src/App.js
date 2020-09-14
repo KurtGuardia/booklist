38cd68cdeeaa621382fc4ba1e0949564b50166ed
Kurt Guardia <kurtguardia@gmail.com>
2020-09-14 10:34:11 +0200

    Created BookList Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import Navbar from './components/components/Navbar';
 import BookContextProvider from './contexts/BookContext';
 import './App.css';
+import BookList from './components/components/BookList';
+
 function App() {
   return (
     <div className='App'>
       <BookContextProvider>
         <Navbar />
+        <BookList />
       </BookContextProvider>
     </div>
   );
